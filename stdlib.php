610ce9c30c90fc5af580b0dff1141412921abf8a
marrtins <marrtins@9aafe359-33b1-4a26-9304-2dc4e408296d>
2019-11-06 22:53:57 +0000

    ibase_table_info() cleanup; ibase_isql() defaults to stdout

--- stdlib.php
+++ stdlib.php
@@ -1436,16 +1436,11 @@ function within($v, $s, $e){
 }
 
 function build_sql($fields, $DATA = null){
-	$values = [];
 	foreach($fields as $k){
-		if(isset($DATA->{$k})){
-			$values[] = $DATA->{$k};
-		} else {
-			$values[] = null;
-		}
+		$values[] = $DATA->{$k} ?? null;
 	}
 
-	return [join(",", $fields), join(",", array_fill(0, count($fields), "?")), $values];
+	return [join(",", $fields), join(",", array_fill(0, count($fields), "?")), $values??[]];
 }
 
 function build_sql_set($fields, $DATA){
