e058091ee0f46cadff1cf57482510efd09944920
marrtins <marrtins@9aafe359-33b1-4a26-9304-2dc4e408296d>
2020-02-02 14:54:42 +0000

    Template cleanup

--- dqdp/Template.php
+++ dqdp/Template.php
@@ -8,6 +8,7 @@ class Template extends TemplateBlock
 	var $root_dir = '.';
 
 	function __construct($root_dir = '.'){
+		//parent::__construct(NULL, )
 		$this->set_root($root_dir);
 	}
 
@@ -18,8 +19,9 @@ class Template extends TemplateBlock
 
 	private function __filename($file_name){
 		$file_name = "$this->root_dir/$file_name";
-		if(!file_exists($file_name))
+		if(!file_exists($file_name)){
 			$this->error('filename: file ['.$file_name.'] does not exists', E_USER_ERROR);
+		}
 
 		return $file_name;
 	}
@@ -29,7 +31,7 @@ class Template extends TemplateBlock
 		$content = '';
 
 		$file_path = $this->__filename($file_name);
-		if($file_exists = file_exists($file_path)){
+		if(file_exists($file_path)){
 			$modtime = filemtime($file_path);
 			$content = file_get_contents($file_path);
 		}
