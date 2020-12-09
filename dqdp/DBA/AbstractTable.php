1759b9100bc38a0af494e0ebdaf04c4c46549a6b
Martins Lazdans <marrtins@dqdp.net>
2020-12-09 09:45:28 +0200

    Cleanup

--- dqdp/DBA/AbstractTable.php
+++ dqdp/DBA/AbstractTable.php
@@ -5,22 +5,6 @@ declare(strict_types = 1);
 namespace dqdp\DBA;
 
 abstract class AbstractTable {
-	// protected $PK;
-	// protected string $Name;
-	// protected ?string $Gen;
-
-	// function getPK(){
-	// 	return $this->PK;
-	// }
-
-	// function getName(){
-	// 	return $this->Name;
-	// }
-
-	// function getGen(){
-	// 	return $this->Gen;
-	// }
-
 	abstract function getPK();
 	abstract function getName(): string;
 	abstract function getGen(): ?string;
