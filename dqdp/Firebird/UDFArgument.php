f7819c9b84d5cf5c76d03a7cc5c7deaa7b09d3b5
Martins Lazdans <marrtins@dqdp.net>
2021-05-26 04:30:29 +0300

    UDFArgument fields

--- dqdp/Firebird/UDFArgument.php
+++ dqdp/Firebird/UDFArgument.php
@@ -6,6 +6,29 @@ namespace dqdp\FireBird;
 
 use dqdp\SQL\Select;
 
+// DECLARE EXTERNAL FUNCTION funcname
+//   [{ <arg_desc_list> | ( <arg_desc_list> ) }]
+//   RETURNS { <return_value> | ( <return_value> ) }
+//   ENTRY_POINT 'entry_point' MODULE_NAME 'library_name'
+
+// <arg_desc_list> ::=
+//   <arg_type_decl> [, <arg_type_decl> ...]
+
+// <arg_type_decl> ::=
+//   <udf_data_type> [BY {DESCRIPTOR | SCALAR_ARRAY} | NULL]
+
+// <udf_data_type> ::=
+//     <scalar_datatype>
+//   | BLOB
+//   | CSTRING(length) [ CHARACTER SET charset ]
+
+// <scalar_datatype> ::=
+//   !! See Scalar Data Types Syntax !!
+
+// <return_value> ::=
+//   { <udf_data_type> | PARAMETER param_num }
+//   [{ BY VALUE | BY DESCRIPTOR [FREE_IT] | FREE_IT }]
+
 class UDFArgument extends Field
 {
 	const MECHANISM_VALUE                = 0;
@@ -19,12 +42,13 @@ class UDFArgument extends Field
 
 	function __construct(UDF $UDF, $name){
 		$this->UDF = $UDF;
-		parent::__construct($UDF->getDb(), $name);
+		parent::__construct($UDF->getDb(), "$name");
 	}
 
 	static function getSQL(): Select {
-		return (new Select('fa.*, cs.RDB$BYTES_PER_CHARACTER'))
+		return (new Select('fa.*, c.*, cs.*, NULL AS COMPUTED_SOURCE'))
 		->From('RDB$FUNCTION_ARGUMENTS fa')
+		->LeftJoin('RDB$COLLATIONS c', '(c.RDB$COLLATION_ID = fa.RDB$COLLATION_ID AND c.RDB$CHARACTER_SET_ID = fa.RDB$CHARACTER_SET_ID)')
 		->LeftJoin('RDB$CHARACTER_SETS cs', 'cs.RDB$CHARACTER_SET_ID = fa.RDB$CHARACTER_SET_ID')
 		->OrderBy('fa.RDB$ARGUMENT_POSITION')
 		;
@@ -42,39 +66,45 @@ class UDFArgument extends Field
 	function ddl(): string {
 		$ddl = '';
 
-		$MT = $this->getMetadata();
-		$FMT = $this->UDF->getMetadata();
-		$ddl = parent::ddl();
+		$MD = $this->getMetadata();
+		$UDFMD = $this->UDF->getMetadata();
+		$parts = $this->ddlParts();
+
+		if(isset($parts['domainname'])){
+			$ddl = $parts['domainname'];
+		} else {
+			$ddl = $parts['datatype'];
+		}
 
 		$paramType = "";
-		if($MT->MECHANISM == UDFArgument::MECHANISM_VALUE){
-			$paramType = " BY VALUE";
-		} elseif($MT->MECHANISM == UDFArgument::MECHANISM_REFERENCE){
+		if($MD->MECHANISM == UDFArgument::MECHANISM_VALUE){
+			$paramType = "BY VALUE";
+		} elseif($MD->MECHANISM == UDFArgument::MECHANISM_REFERENCE){
 			// $paramType = " BY REFERENCE";
-		} elseif($MT->MECHANISM == UDFArgument::MECHANISM_DESCRIPTOR){
-			$paramType = " BY DESCRIPTOR";
-		} elseif($MT->MECHANISM == UDFArgument::MECHANISM_ARRAY_DESCRIPTOR){
-			$paramType = " BY SCALAR_ARRAY";
-		} elseif($MT->MECHANISM == UDFArgument::MECHANISM_NULL){
-			$paramType = " NULL";
+		} elseif($MD->MECHANISM == UDFArgument::MECHANISM_DESCRIPTOR){
+			$paramType = "BY DESCRIPTOR";
+		} elseif($MD->MECHANISM == UDFArgument::MECHANISM_ARRAY_DESCRIPTOR){
+			$paramType = "BY SCALAR_ARRAY";
+		} elseif($MD->MECHANISM == UDFArgument::MECHANISM_NULL){
+			$paramType = "NULL";
 		} else {
-			trigger_error("Unknown MECHANISM: $MT->MECHANISM");
-			dumpr($MT);
+			trigger_error("Unknown MECHANISM: $MD->MECHANISM");
 		}
 
 		# Returning argument
-		if($MT->ARGUMENT_POSITION == $FMT->RETURN_ARGUMENT){
-			if($FMT->RETURN_ARGUMENT){
-				$ddl = "RETURNS PARAMETER {$FMT->RETURN_ARGUMENT}";
+		if($MD->ARGUMENT_POSITION == $UDFMD->RETURN_ARGUMENT){
+			if($UDFMD->RETURN_ARGUMENT){
+				$ddl = "RETURNS PARAMETER {$UDFMD->RETURN_ARGUMENT}";
 			} else {
-				$ddl = "RETURNS {$ddl}{$paramType}";
+				$ddl = "RETURNS $ddl $paramType";
 
-				if($MT->MECHANISM < 0){
+				if($MD->MECHANISM < 0){
 					$ddl .= " FREE_IT";
 				}
 			}
 		} else {
-			$ddl = "{$ddl}{$paramType}";
+			$ddl = "$ddl $paramType";
+			// $ddl = "{$ddl}{$paramType}";
 		}
 
 		return $ddl;
