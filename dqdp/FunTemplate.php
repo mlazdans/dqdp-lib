59e8f941503f26072c37ea2bda4f2a69a516a0b2
Martins Lazdans <marrtins@dqdp.net>
2022-05-14 01:30:46 +0300

    Add FunTemplate experiment

---
+++ dqdp/FunTemplate.php
@@ -0,0 +1,18 @@
+<?php
+
+namespace dqdp;
+
+class FunTemplate extends FunTemplateBlock
+{
+	var $mod_time;
+
+	function __construct($file_name){
+		if(!file_exists($file_name)){
+			$this->error("file not found ($file_name)", E_USER_ERROR);
+		}
+
+		$this->mod_time = filemtime($file_name);
+
+		parent::__construct(NULL, $file_name, file_get_contents($file_name));
+	}
+}
