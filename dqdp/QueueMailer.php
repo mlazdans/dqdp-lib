ba664d1ff45b159b3a48cb4c32b85476eb7324ab
Martins Lazdans <marrtins@dqdp.net>
2022-05-17 13:08:36 +0300

    Remove query array arg, cleanup, strict

--- dqdp/QueueMailer.php
+++ dqdp/QueueMailer.php
@@ -1,4 +1,4 @@
-<?php
+<?php declare(strict_types = 1);
 
 namespace dqdp;
 
@@ -55,17 +55,6 @@ class QueueMailer extends PHPMailer implements TransactionInterface
 		$Ent = (new QueueMailer\Entity)->set_trans($this->get_trans());
 
 		return $Ent->save($DATA);
-
-		// $fields = array_keys($DATA);
-
-		// list($fieldSQL, $valuesSQL, $values) = build_sql($fields, (object)$DATA, true);
-		// $sql = "INSERT INTO MAIL_QUEUE (ID,CREATE_TIME,TIME_TO_SEND,$fieldSQL) VALUES (NEXT VALUE FOR GEN_MAIL_QUEUE_ID,CURRENT_TIMESTAMP,CURRENT_TIMESTAMP,$valuesSQL) RETURNING ID";
-
-		// if($q = $this->get_trans()->query($sql, $values)){
-		// 	$r = $this->get_trans()->fetch($q);
-		// 	return $r->ID;
-		// }
-		// return false;
 	}
 
 	function getQueue(){
@@ -100,14 +89,14 @@ class QueueMailer extends PHPMailer implements TransactionInterface
 		try {
 			if($this->smtpSend($r->MIME_HEADERS,$r->MIME_BODY)){
 				$sql = "UPDATE MAIL_QUEUE SET SENT_TIME = CURRENT_TIMESTAMP, ERROR_MSG = NULL WHERE ID = ?";
-				$this->get_trans()->query($sql, [$r->ID]);
+				$this->get_trans()->query($sql, $r->ID);
 
 				return true;
 			}
 		} catch (Exception $e) {
 			$this->ErrorInfo = $e->getMessage();
 			$sql = "UPDATE MAIL_QUEUE SET TRY_SENT = TRY_SENT + 1, ERROR_MSG = ? WHERE ID = ?";
-			$this->get_trans()->query($sql, [$e->getMessage(), $r->ID]);
+			$this->get_trans()->query($sql, $e->getMessage(), $r->ID);
 
 			return false;
 		}
