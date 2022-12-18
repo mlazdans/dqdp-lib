639395def18f73c1ad1d3cc4db308733b3cf9d66
Martins Lazdans <marrtins@dqdp.net>
2022-12-18 13:34:16 +0200

    Remove DataObject dependency

--- dqdp/DBA/interfaces/DataMapperInterface.php
+++ dqdp/DBA/interfaces/DataMapperInterface.php
@@ -1,14 +1,12 @@
 <?php declare(strict_types = 1);
 
 namespace dqdp\DBA\interfaces;
-
-use dqdp\DBA\DataObject;
 use stdClass;
 
 interface DataMapperInterface {
 	function __construct(?iterable $data, ?iterable $defaults = null);
 	function initPoperty($v, $k);
 	function toDBObject(): stdClass;
-	static function withDefaults(): DataObject;
-	static function fromDBObject(stdClass $o): DataObject;
+	static function withDefaults(): iterable;
+	static function fromDBObject(stdClass $o): iterable;
 }
