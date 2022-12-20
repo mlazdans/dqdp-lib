7ed7ac0c091782ae6b9a213615b27f9f5cdda2a3
Martins Lazdans <marrtins@dqdp.net>
2022-12-20 18:37:31 +0200

    Refactor API class, add serialization method

--- dqdp/API/API.php
+++ dqdp/API/API.php
@@ -1,17 +1,20 @@
 <?php declare(strict_types = 1);
 
-namespace dqdp;
+namespace dqdp\API;
 
 abstract class API {
 	static string $URL;
-	static string $KEY;
-	static string $SID;
+	static SerializationMethod $serializationMethod = SerializationMethod::JSON;
 	static string $RAW_RESPONSE_DATA;
 
 	abstract static function set_host(string $host): void;
 
 	static function decode_response(string $data): mixed {
-		return json_decode(self::$RAW_RESPONSE_DATA = $data);
+		if(static::$serializationMethod == SerializationMethod::JSON){
+			return json_decode(static::$RAW_RESPONSE_DATA = $data);
+		} elseif(static::$serializationMethod == SerializationMethod::PHP){
+			return unserialize(static::$RAW_RESPONSE_DATA = $data);
+		}
 	}
 
 	static function post(string $url, array $data = []): mixed {
@@ -19,26 +22,13 @@ abstract class API {
 			return false;
 		}
 
-		if(static_prop_is_initialized(self::class, 'SID')){
-			$data['SID'] = self::$SID;
-		}
-
-		curl_setopt($ch, CURLOPT_URL, self::$URL.$url);
+		curl_setopt($ch, CURLOPT_URL, static::$URL.$url);
 		curl_setopt($ch, CURLOPT_POST, true);
 		curl_setopt($ch, CURLOPT_POSTFIELDS, http_build_query($data));
 		curl_setopt($ch, CURLOPT_RETURNTRANSFER, 1);
 		$HTML = curl_exec($ch);
 		curl_close($ch);
 
-		return self::decode_response($HTML);
-	}
-
-	# TODO: remove
-	static function set_sid(string $SID): void {
-		self::$SID = $SID;
-	}
-
-	static function set_key(string $KEY): void {
-		self::$SID = $KEY;
+		return static::decode_response($HTML);
 	}
 }
