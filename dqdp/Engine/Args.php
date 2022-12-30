2a62561ba525983f3653a36f4e51c80deaef339b
Martins Lazdans <marrtins@dqdp.net>
2022-12-30 17:17:30 +0200

    Add constructor

--- dqdp/Engine/Args.php
+++ dqdp/Engine/Args.php
@@ -2,10 +2,32 @@
 
 namespace dqdp\Engine;
 
-use dqdp\StricStdObject;
 use dqdp\TraversableConstructor;
+use stdClass;
 
-class Args extends StricStdObject implements TraversableConstructor {
+class Args extends stdClass implements TraversableConstructor {
+	function __construct(array|object|null $data = null, array|object|null $defaults = null){
+		$looper = function(array|object|null $o = null): void {
+			if(is_null($o)) {
+				return;
+			}
+
+			$o_data = is_array($o) ? $o : get_object_public_vars($o);
+			foreach($o_data as $k=>$v){
+				if(!isset($this->$k)){
+					$this->$k = $v;
+				}
+			}
+		};
+
+		$looper($data);
+		$looper($defaults);
+	}
+
+	static function initFrom(array|object|null $data = null, array|object|null $defaults = null): static {
+		return new static($data, $defaults);
+	}
+
 	// function set(int|string $k, mixed $v): static {
 	// 	$this->$k = $v;
 	// 	return $this;
