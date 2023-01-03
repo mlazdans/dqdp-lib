00a46cd1e8ebe20ba9eaebee4778dcf93cd4c1d3
Martins Lazdans <marrtins@dqdp.net>
2023-01-04 01:59:58 +0200

    Make filter properties writable

--- dqdp/Settings/SettingsFilter.php
+++ dqdp/Settings/SettingsFilter.php
@@ -7,15 +7,13 @@ use dqdp\SQL\Select;
 
 class SettingsFilter extends AbstractFilter {
 	function __construct(
-		readonly ?string $SET_DOMAIN = null,
-		readonly ?string $SET_KEY = null,
-	) {
-	}
+		public ?string $SET_DOMAIN = null,
+		public ?string $SET_KEY = null,
+	) { }
 
 	function apply(Select $sql): Select {
 		$this->apply_fields_with_values($sql, ['SET_KEY', 'SET_DOMAIN']);
 
 		return $sql;
 	}
-
 }
