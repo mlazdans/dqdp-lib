d3cb2d1b4428ea1cd0224921ae73262d7f93a96f
Martins Lazdans <marrtins@dqdp.net>
2023-01-08 13:19:10 +0200

    Add MQ_IDS filter

--- dqdp/MailQueue/MailQueueFilter.php
+++ dqdp/MailQueue/MailQueueFilter.php
@@ -5,12 +5,14 @@ namespace dqdp\MailQueue;
 use dqdp\DBA\AbstractFilter;
 use dqdp\SQL\Condition;
 use dqdp\SQL\Select;
+use InvalidArgumentException;
 
 class MailQueueFilter extends AbstractFilter {
 	function __construct(
 		public ?int $ID = null,
 		public ?bool $IS_SENT = null,
 		public ?bool $IS_ERRORED = null,
+		public ?array $MQ_IDS = null,
 	) {}
 
 	function apply_filter(Select $sql): Select {
@@ -23,6 +25,13 @@ class MailQueueFilter extends AbstractFilter {
 
 		if(isset($this->IS_ERRORED)){
 			$Cond->add_condition($this->IS_ERRORED ? 'TRY_SENT > 0' : 'TRY_SENT = 0');
+		}
+
+		if($this->MQ_IDS !== null){
+			if(empty($this->MQ_IDS)){
+				throw new InvalidArgumentException('Norādīts tukšs $MQ_IDS[]');
+			}
+			$sql->WhereIn("ID", $this->MQ_IDS);
 		}
 
 		if($Cond->non_empty()){
