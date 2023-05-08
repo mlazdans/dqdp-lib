dd1b774623c4872b6517e453a02edcfc35751443
Martins Lazdans <marrtins@dqdp.net>
2023-05-08 21:21:08 +0300

    Remove mod_time

--- dqdp/Template.php
+++ dqdp/Template.php
@@ -4,14 +4,10 @@ namespace dqdp;
 
 class Template extends TemplateBlock
 {
-	var $mod_time;
-
 	function __construct($file_name){
 		if(!file_exists($file_name)){
 			$this->error("file not found ($file_name)", E_USER_ERROR);
 		}
-
-		$this->mod_time = filemtime($file_name);
 
 		parent::__construct(NULL, $file_name, file_get_contents($file_name));
 	}
